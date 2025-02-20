25204aef8b7b4131fd5a3f76ebbc5e471e4eec7d
EnigmaByte7 <saxenay117@gmail.com>
2025-02-21 03:01:38 +0530

    merge conflits

--- src/Router.jsx
+++ src/Router.jsx
@@ -5,15 +5,26 @@ import Booking from "./pages/Booking";
 import Tickets from "./pages/Tickets";
 import Scanner from "./pages/Scanner";
 import Dashboard from "./pages/Dashboard";
+import SignUp from "./pages/SignUp";
+import { getAuth } from "firebase/auth"; 
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ element }) => {
+  const auth = getAuth();
+  const user = auth.currentUser;
+  return user ? element : <Navigate to="/signup" replace />;
+};
+
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/Booking" element={<Booking />} />
-      <Route path="/tickets" element={<Tickets />} />
+      <Route path="/tickets" element={<ProtectedRoute element={<Tickets />} />} />
       <Route path="/scanner" element={<Scanner />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
 };
