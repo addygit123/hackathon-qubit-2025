91a4ed453f458a0e10b227685aca4d65234c520b
Adarsh <minecraftaddy123@gmail.com>
2025-02-20 14:53:54 +0530

    Added firebase

---
+++ backend/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB5l1iX-0cpztFDN4QDsRLcL_MInL-EfqE",
+  authDomain: "qubit-951c9.firebaseapp.com",
+  projectId: "qubit-951c9",
+  storageBucket: "qubit-951c9.firebasestorage.app",
+  messagingSenderId: "925345673096",
+  appId: "1:925345673096:web:e25b607127db9e9812d43d",
+  measurementId: "G-RZDJKP6TX1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
